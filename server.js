b2fa08ccdad16a1b89e8ee44bbfd6b6317b3d9aa
Nick Gonzalez <gonzaleznick5@gmail.com>
2019-10-07 17:46:35 -0400

    Server running w/ a 200 status test

--- server.js
+++ server.js
@@ -6,4 +6,11 @@ const server = express();
 
 server.use(express.json());
 
+
+//Test request 
+
+server.get('/', (req,res)=> {
+    res.status(200).json({message: "In there like swim wear"});
+})
+
 module.exports = server;
