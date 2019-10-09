66de46e8c4a5987d9949a929f4b82ada6b9de664
Nick Gonzalez <gonzaleznick5@gmail.com>
2019-10-08 22:31:27 -0400

    MVP and two of the 3 stretches

--- accounts/accounts-router.js
+++ accounts/accounts-router.js
@@ -6,8 +6,11 @@ const db = require('../data/dbConfig');
 
 const router = express.Router()
 
+
+// GET - for all data
 router.get('/', (req, res) => {
     db.select('*').from('accounts')
+    
     .then(accounts =>{
         res.status(200).json(accounts);
     })
@@ -15,23 +18,26 @@ router.get('/', (req, res) => {
         res.status(500).json(error);
     })
    });
-   
+
+
+// GET - request by ID    
    router.get('/:id', (req, res) => {
    // select * from posts where id = req.params.id
   db.select('*')
   .from('accounts')
   .where('id', '=', req.params.id)
-  .first()
+  
   .then(account => {
-    // send the post to the client
+    //Post request to client 
     res.status(200).json(account);
   })
   .catch(error => {
-    // remember to handle the error
+    //Handle Error 
     res.status(500).json(error);
    });
 });
-   
+  
+// POST - request 
    router.post('/', (req, res) => {
        const accountData = req.body;
    db('accounts')
@@ -43,13 +49,34 @@ router.get('/', (req, res) => {
        res.status(500).json(error);
    })
    });
-   
+
+   //PUT - Request 
    router.put('/:id', (req, res) => {
-   
+    db('accounts')
+    .where({ id: req.params.id })
+    .update(req.body)
+    .then(count => {
+      res.status(200).json(count);
+    })
+    .catch(error => {
+      // remember to handle the error
+      res.status(500).json(error);
+    });
    });
    
+
+   // DELETE REQUEST 
    router.delete('/:id', (req, res) => {
-   
+    db('accounts')
+    .where({ id: req.params.id })
+    .del()
+    .then(count => {
+      res.status(200).json(count);
+    })
+    .catch(error => {
+      // remember to handle the error
+      res.status(500).json(error);
+    });
    });
 
 
